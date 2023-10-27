a8fc65877c7d9c81e141795d8094051371867a98
LuisVenturaH <l_ventur@yahoo.com>
2023-10-27 13:50:08 +0200

    Ejercicio 16 resuelto

--- ejercicio16/ejercicio16.js
+++ ejercicio16/ejercicio16.js
@@ -5,8 +5,16 @@
  *  con el resultado de estos nueve numeros ([1,4,9...])
  */
 
+
 function cuadraroNumerosNaturales() {
-  //Escribe tu codigo aqui
+  let numerosNat = [];             // Creo una variable para almacenar los elementos del array
+  for (let i=0; i <= 9; i++) {    // Creo un bucle for para recorrer los numeros del 0 al 9. Incio en 0 y termino en 9 inlcusive
+    let cuadrado = i * i;         // Creo una varable para almacenar los cuadrados del for
+    numerosNat.push(cuadrado);     // Agreago los numeros del cuadrado dentro del array con el metodo push, que sirve para 
+}                                 // incluir elementos dentro de un array
+
+return numerosNat;
+  
 }
 
 module.exports = { cuadraroNumerosNaturales };
